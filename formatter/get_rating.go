8716af9c96b959ebb246ddcce35f3192982cc1f2
orel8654 <egorao@mail.ru>
2023-09-12 14:33:35 +0700

    refactoring

--- formatter/get_rating.go
+++ formatter/get_rating.go
@@ -1,82 +1,90 @@
 package formatter
 
+//Добавление
+//error wrap
+//multierr
+//time.Time при чтении
+
 import (
 	"encoding/csv"
 	"encoding/json"
 	"fmt"
-	"io"
-	"log"
 	"net/http"
 	"os"
 
 	"github.com/gocarina/gocsv"
 )
 
-const Maxi int = 3
+const Buffer = 3
+const (
+	ColID = iota
+	ColLastLogin
+	ColDateJoined
+	ColUsername
+	ColFirstName
+	ColPhone
+	ColCountCard
+	ColProductID
+)
 
 type FileCSV struct {
-	Id         string
-	LastLogin  string
-	DateJoined string
-	Username   string
-	FirstName  string
-	Phone      string
-	CountCard  string
-	ProductId  string
-	Rating     any
-	url        string
+	ID,
+	LastLogin,
+	DateJoined,
+	Username,
+	FirstName,
+	Phone,
+	CountCard,
+	ProductID,
+	url string
+	Rating any
 }
 
 type ResponseData struct {
-	AvItemId int64   `json:"av_item_id"`
+	AvItemID int64   `json:"av_item_id"`
 	AvComps  int64   `json:"av_comps"`
 	AvRating float64 `json:"av_rating"`
 }
 
-func NewFetch(id int, work <-chan FileCSV, result chan<- FileCSV) {
+func fetcByURL(url string) (data ResponseData, err error) {
+	fmt.Println(url)
+	response, err := http.Get(url)
+	if err != nil {
+		return ResponseData{}, err
+	}
+	defer response.Body.Close()
+	return data, json.NewDecoder(response.Body).Decode(&data)
+}
+
+func Fetch(id int, work <-chan FileCSV, result chan<- FileCSV) {
 	for c := range work {
-		if c.ProductId == "" {
+		if c.ProductID == "" {
 			c.Rating = 0
 			result <- c
-		} else {
-			fmt.Println(c.url)
-			response, err := http.Get(c.url)
-			c.Rating = 0
-			if err != nil {
-				fmt.Println(err)
-				result <- c
-			}
-			defer response.Body.Close()
-			body, err := io.ReadAll(response.Body)
-			if err != nil {
-				fmt.Println(err)
-				result <- c
-			}
-			var data ResponseData
-			err = json.Unmarshal(body, &data)
-			if err != nil {
-				fmt.Println(err)
-				result <- c
-			}
-			c.Rating = data.AvRating
-			result <- c
+			continue
 		}
+		data, err := fetcByURL(c.url)
+		if err != nil {
+			fmt.Println(err)
+		}
+		c.Rating = data.AvRating
+		result <- c
 	}
 }
 
 func Getter(data []FileCSV) []FileCSV {
 	var count int64
-	worker := make(chan FileCSV, len(data))
-	result := make(chan FileCSV, len(data))
+	worker := make(chan FileCSV, Buffer)
+	result := make(chan FileCSV, Buffer)
 
-	newFileCSV := []FileCSV{}
+	newFileCSV := make([]FileCSV, 0, len(data))
 
-	for i := 1; i <= Maxi; i++ {
-		go NewFetch(i, worker, result)
+	for i := 1; i <= Buffer; i++ {
+		go Fetch(i, worker, result)
 	}
 
 	for _, val := range data {
-		val.url = "http://127.0.0.1:8000/api/v3/rating?marketplace=wb&item_id=" + string(val.ProductId)
+		val.url = "http://127.0.0.1:8000/api/v3/rating?marketplace=wb&item_id=" + val.ProductID
 		worker <- val
 	}
 	close(worker)
@@ -89,10 +97,10 @@ func Getter(data []FileCSV) []FileCSV {
 	return newFileCSV
 }
 
-func ReaderCSV(path string) []FileCSV {
+func ReaderCSV(path string) ([]FileCSV, error) {
 	f, err := os.Open(path)
 	if err != nil {
-		log.Fatal("Unable to read input file "+path, err)
+		return nil, err
 	}
 	defer f.Close()
 
@@ -100,27 +108,38 @@ func ReaderCSV(path string) []FileCSV {
 	csvReader.Comma = ';'
 	records, err := csvReader.ReadAll()
 	if err != nil {
-		log.Fatal("Unable to parse file as CSV for "+path, err)
+		return nil, err
 	}
 
-	result := []FileCSV{}
+	result := make([]FileCSV, 0, len(records)-1)
 	for i, line := range records {
-		if i > 0 && len(line) != 0 {
-			rec := FileCSV{Id: line[0], LastLogin: line[1], DateJoined: line[2], Username: line[3], FirstName: line[4], Phone: line[5], CountCard: line[6], ProductId: line[7]}
-			result = append(result, rec)
+		if i == 0 || len(line) == 0 {
+			continue
 		}
+		rec := FileCSV{
+			ID:         line[ColID],
+			LastLogin:  line[ColLastLogin],
+			DateJoined: line[ColDateJoined],
+			Username:   line[ColUsername],
+			FirstName:  line[ColFirstName],
+			Phone:      line[ColPhone],
+			CountCard:  line[ColCountCard],
+			ProductID:  line[ColProductID],
+		}
+		result = append(result, rec)
 	}
-	return result
+	return result, nil
 }
 
-func Writer(data []FileCSV) {
+func Writer(data []FileCSV) error {
 	file, err := os.Create("newResult.csv")
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer file.Close()
 
 	if err := gocsv.MarshalFile(&data, file); err != nil {
-		panic(err)
+		return err
 	}
+	return nil
 }
--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"get_rating_card/formatter"
+	"os"
 )
 
 func main() {
 	var path string
-	fileData := formatter.ReaderCSV(path)
+	fileData, err := formatter.ReaderCSV(path)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 	newFileCSV := formatter.Getter(fileData)
 	formatter.Writer(newFileCSV)
 }
